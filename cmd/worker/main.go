312fa87429b81a2c68c1015f7fdfcabe7520bc2a
mark3748 <mark3748@gmail.com>
2025-09-01 22:05:13 -0600

    Add configurable log path and test-connection logging (#56)
    * config: add LOG_PATH and test connection endpoint
    
    * cmd: handle unwritable log path (#57)

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -6,8 +6,10 @@ import (
 	"embed"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/smtp"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"text/template"
@@ -20,9 +22,9 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
-    "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 
-    "github.com/mark3748/helpdesk-go/internal/sla"
+	"github.com/mark3748/helpdesk-go/internal/sla"
 )
 
 type Config struct {
@@ -43,6 +45,7 @@ type Config struct {
 	MinIOSecret   string
 	MinIOBucket   string
 	MinIOUseSSL   bool
+	LogPath       string
 }
 
 func getEnv(key, def string) string {
@@ -72,6 +75,7 @@ func cfg() Config {
 		MinIOSecret:   getEnv("MINIO_SECRET_KEY", ""),
 		MinIOBucket:   getEnv("MINIO_BUCKET", ""),
 		MinIOUseSSL:   getEnv("MINIO_USE_SSL", "false") == "true",
+		LogPath:       getEnv("LOG_PATH", os.TempDir()),
 	}
 }
 
@@ -169,9 +173,27 @@ func sendEmail(c Config, j EmailJob) error {
 
 func main() {
 	c := cfg()
+	writer := io.Writer(os.Stdout)
 	if c.Env == "dev" {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+		writer = zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	}
+	if err := os.MkdirAll(c.LogPath, 0o755); err != nil {
+		log.Warn().Err(err).Str("dir", c.LogPath).Msg("using stdout for logs")
+	} else {
+		logFile := filepath.Join(c.LogPath, "worker.log")
+		f, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+		if err != nil {
+			log.Warn().Err(err).Str("path", logFile).Msg("using stdout for logs")
+		} else {
+			if c.Env == "dev" {
+				writer = zerolog.MultiLevelWriter(f, writer)
+			} else {
+				writer = f
+			}
+			defer f.Close()
+		}
+	}
+	log.Logger = zerolog.New(writer).With().Timestamp().Logger()
 	ctx := context.Background()
 	db, err := pgxpool.New(ctx, c.DatabaseURL)
 	if err != nil {
