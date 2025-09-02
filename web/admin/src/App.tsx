93c32f7c5708b599f8cea41e24da1618604e65e3
mark3748 <mark3748@gmail.com>
2025-09-01 22:05:13 -0600

    Add configurable log path and test-connection logging (#56)
    * config: add LOG_PATH and test connection endpoint
    
    * cmd: handle unwritable log path (#57)

--- web/admin/src/App.tsx
+++ web/admin/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UserRoles from './components/UserRoles'
+import TestConnection from './components/TestConnection'
 
 export default function App() {
   return (
     <div>
       <h1>Admin</h1>
       <UserRoles />
+      <TestConnection />
     </div>
   )
 }
