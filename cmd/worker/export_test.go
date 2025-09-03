59cd56a18e5330d578ccbd4def8ece0eefe469b0
Mark Hamburger <mark3748@gmail.com>
2025-09-03 16:45:53 -0600

    corrected ci build failures

--- cmd/worker/export_test.go
+++ cmd/worker/export_test.go
@@ -117,10 +117,12 @@ func TestHandleExportTicketsJob(t *testing.T) {
 	if err := json.Unmarshal([]byte(val), &st); err != nil {
 		t.Fatalf("unmarshal: %v", err)
 	}
-	if st.Status != "done" || st.URL == "" {
-		t.Fatalf("unexpected status: %+v", st)
-	}
-	res, err := http.Get(st.URL)
+    if st.Status != "done" || st.ObjectKey == "" {
+        t.Fatalf("unexpected status: %+v", st)
+    }
+    // Construct a fetch URL from the fake store and object key.
+    fetchURL := store.URL() + "/bucket/" + st.ObjectKey
+    res, err := http.Get(fetchURL)
 	if err != nil {
 		t.Fatalf("download: %v", err)
 	}
